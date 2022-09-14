ca502bd64e2c652ef4fad626215ee946ffb836b8
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2022-09-14 11:51:27 +0200

    Fix build errors

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,16 +1,9 @@
 import React from "react";
-import Layout from "@/Layouts/Authenticated";
-import { Head } from "@inertiajs/inertia-react";
+import Layout from "@/Layouts/Layout";
 
 export default function Dashboard(props) {
     return (
-        <Layout
-            auth={props.auth}
-            errors={props.errors}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
-        >
-            <Head title="Dashboard" />
-
+        <Layout auth={props.auth} errors={props.errors} title="Dashboard">
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
