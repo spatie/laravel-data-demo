c5a97909bb2d64313cbe7784f0c0d97f539434b4
Freek Van der Herten <freek@spatie.be>
2022-09-14 21:44:28 +0200

    wip

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -45,7 +45,7 @@ class DatabaseSeeder extends Seeder
 
         Contact::factory()->create([
             'name' => 'Christoph Rumpel',
-            'email' => 'christoph@hat-factory.com'
+            'email' => 'christoph@hero-of-the-world.au'
         ]);
 
         Contact::factory()->create([
