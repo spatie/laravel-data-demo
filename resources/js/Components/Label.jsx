3553a4cba73171b275bd8a5ca294e2f59eb36442
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2022-09-14 11:52:06 +0200

    Label styling

--- resources/js/Components/Label.jsx
+++ resources/js/Components/Label.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Label({ forInput, value, className, children }) {
     return (
-        <label htmlFor={forInput} className={`block font-medium text-sm text-gray-700 ` + className}>
+        <label htmlFor={forInput} className={`block font-medium text-xs uppercase tracking-wide text-gray-400 ` + className}>
             {value ? value : children}
         </label>
     );
