2236b22d370a5f1e59bbe53a60d597128a95afed
Freek Van der Herten <freek@spatie.be>
2023-06-26 14:07:12 +0200

    Create ContactData class with id, name, email, address, postal, and city properties

---
+++ app/Data/ContactData.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Data;
+
+use Spatie\LaravelData\Attributes\Validation\Rule;
+use Spatie\LaravelData\Data;
+
+class ContactData extends Data
+{
+    public function __construct(
+        public int $id,
+        public string $name,
+        public string $email,
+        public string $address,
+        public string $postal,
+        public string $city,
+    )
+    {
+    }
+}
+
