9a94b5f7014fddb66583edff66277e22370944e0
Shift <shift@laravelshift.com>
2023-06-28 11:05:39 +0000

    Shift cleanup

--- app/Http/Controllers/Contacts/ContactsController.php
+++ app/Http/Controllers/Contacts/ContactsController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers\Contacts;
 
-use Inertia\Response;
-use Illuminate\Http\RedirectResponse;
 use App\Http\Requests\EditContactRequest;
 use App\Models\Contact;
+use Illuminate\Http\RedirectResponse;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class ContactsController
 {
